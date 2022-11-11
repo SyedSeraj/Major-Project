fcea85834da621a47591a06d56fc4ac30221a7e2
SyedSeraj <amaanhusaini22@gmail.com>
2022-11-11 11:52:29 +0530

    button

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	return <div>    
+		<div className="container"> 
+	<div className='font' >Who Are You?</div>
+  <div className='center'><button class="btn" id="btn-1">Manufacturer</button>
+  <button class="btn" id="btn-2">Supplier</button>
+  <button class="btn" id="btn-3">Customer</button></div>
+  </div> </div>;
 };
 
 export default Home;
